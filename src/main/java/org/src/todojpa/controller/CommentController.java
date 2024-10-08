a12168dab45c3864c803b510b54da6b103d7382d
kimwanyoung <dhks2869@gmail.com>
2024-10-08 15:06:54 +0900

    feat : 댓글 전체 조회

--- src/main/java/org/src/todojpa/controller/CommentController.java
+++ src/main/java/org/src/todojpa/controller/CommentController.java
@@ -1,6 +1,9 @@
 package org.src.todojpa.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedModel;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.src.todojpa.domain.dto.CommentResponseDto;
@@ -12,6 +15,13 @@ import org.src.todojpa.service.CommentService;
 public class CommentController {
 
     private final CommentService commentService;
+
+    @GetMapping
+    public ResponseEntity<PagedModel<CommentResponseDto>> retrieveComments(@PathVariable Long scheduleId, Pageable pageable) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(new PagedModel<>(this.commentService.retrieveComments(scheduleId, pageable)));
+    }
 
     @GetMapping("/{commentId}")
     public ResponseEntity<CommentResponseDto> retrieveComment(
--- src/main/java/org/src/todojpa/service/CommentService.java
+++ src/main/java/org/src/todojpa/service/CommentService.java
@@ -1,10 +1,15 @@
 package org.src.todojpa.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.src.todojpa.domain.dto.CommentResponseDto;
 import org.src.todojpa.domain.entity.Comment;
 import org.src.todojpa.repository.CommentRepository;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -12,6 +17,23 @@ public class CommentService {
 
     private final CommentRepository commentRepository;
     private final ScheduleService scheduleService;
+
+    public Page<CommentResponseDto> retrieveComments(Long scheduleId, Pageable pageable) {
+        this.scheduleService.validateScheduleExists(scheduleId);
+        Page<Comment> comments = this.commentRepository.findAll(pageable);
+
+        List<CommentResponseDto> commentResponseDtos = comments.getContent().stream()
+                .map(comment -> CommentResponseDto.builder()
+                        .id(comment.getId())
+                        .username(comment.getUsername())
+                        .contents(comment.getContents())
+                        .createdAt(comment.getCreatedAt())
+                        .modifiedAt(comment.getModifiedAt())
+                        .build())
+                .toList();
+
+        return new PageImpl<>(commentResponseDtos, pageable, comments.getTotalPages());
+    }
 
     public CommentResponseDto retrieveCommentById(Long scheduleId, Long commentId) {
         this.scheduleService.validateScheduleExists(scheduleId);
